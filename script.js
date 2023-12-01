91f8c9a9acfb66e656952c93c2768e6420a1b129
Richard Barnitz <richardbarnitz@gmail.com>
2023-11-30 17:39:22 -0800

    one leetcode challenge finished: palindromes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// First Problem
+// ------- First Problem ---------
 
 // Companies
 // Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
@@ -31,3 +31,91 @@
 // 1 <= word1[i].length, word2[i].length <= 103
 // 1 <= sum(word1[i].length), sum(word2[i].length) <= 103
 // word1[i] and word2[i] consist of lowercase letters.
+
+
+// ------- Second Problem ---------
+
+
+//Two methods, make reverse string and compare to orginal, 
+//or, divide it in half and see if halves compare.
+
+function palindrome(variable1){
+    console.log(variable1);
+
+    let wordstr = variable1.toString();
+    
+
+
+    let length = variable1.length;
+    let backwards = [];
+
+    //read through letters in word
+
+    for (i = length - 1; i >= 0; i--) {
+       console.log(variable1[i]);
+       backwards.push(variable1[i]);
+
+    //create a backwards word with each letter  (backwards.push())
+    }
+    let newWord  = backwards.join('');
+    console.log(newWord);
+    console.log(variable1);
+
+    return variable1 === newWord;
+}
+
+let result1 = palindrome('racecar');
+console.log(result1);
+
+/*
+
+
+    if (backwards === variable1) {
+        console.log(true);
+    }
+        else{
+
+        console.log(false);
+        }
+
+
+    
+
+}
+*/
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+// Example 1:
+
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+ 
+
+// Follow up: Could you solve it without converting the integer to a string?
+// Accepted
+// 3.9M
+// Submissions
+// 7.1M
+// Acceptance Rate
+// 54.9%
